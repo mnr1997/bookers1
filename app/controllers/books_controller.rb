ee029b453ab4ce1f6e073bdf762726e60badd777
EC2 Default User <ec2-user@ip-172-31-11-252.ap-northeast-1.compute.internal>
2024-04-09 17:35:48 +0000

    [finish]新規投稿をindexに表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,20 +2,21 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      flash.now[:notice] = "Book wasn't successfully created."
-      render :new
+      @books = Book.all
+      render :index
     end
   end
-  
+
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -25,26 +26,25 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
-      flash.now[:notice] = "Book wasn't successfully updated."
       render :edit
     end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
