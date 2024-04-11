e0b244f17f71f0d22b6de28c60e268af4488ac36
EC2 Default User <ec2-user@ip-172-31-11-252.ap-northeast-1.compute.internal>
2024-04-11 05:04:24 +0000

    [finish]bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books
-  post 'books' => 'books#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
